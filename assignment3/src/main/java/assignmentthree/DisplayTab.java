4d3d2053a010ed2ec5ad3e6ec981bd67c8478eaa
Anukrati Verma <averma@avs-macbook.local>
2024-05-17 12:40:44 -0400

    added MenuItem and OrderItem classes

---
+++ assignment3/src/main/java/assignmentthree/DisplayTab.java
@@ -0,0 +1,13 @@
+package assignmentthree;
+
+public class DisplayTab implements Command{
+    Aggregator aggregator;
+
+    public DisplayTab(Aggregator aggregator){
+        this.aggregator = aggregator;
+    }
+
+    public void execute() {
+        // display tab code
+    }
+}
---
+++ assignment3/src/main/java/assignmentthree/OrderItem.java
@@ -0,0 +1,13 @@
+package assignmentthree;
+
+public class OrderItem {
+    int itemNum;
+    
+    public OrderItem(int itemNum) {
+        this.itemNum = itemNum;
+    }
+    
+    public int getItemNum() {
+        return itemNum;
+    }
+}
