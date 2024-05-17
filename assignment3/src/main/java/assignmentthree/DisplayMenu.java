651c4bf1426bb098d14a622befc8ffaa55f1e8c8
Anukrati Verma <averma@avs-macbook.local>
2024-05-17 12:12:38 -0400

    added command classes

---
+++ assignment3/src/main/java/assignmentthree/DisplayMenu.java
@@ -0,0 +1,13 @@
+package assignmentthree;
+
+public class DisplayMenu implements Command{
+    Aggregator aggregator;
+
+    public DisplayMenu(Aggregator aggregator){
+        this.aggregator = aggregator;
+    }
+
+    public void execute() {
+        // display menu code
+    }
+}
